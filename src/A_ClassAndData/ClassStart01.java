530122219582d2aefcf36bd2865e8d68970dc42a
OurOh <ahn980321@gmail.com>
2024-08-25 23:31:21 +0900

    0825 commit test

---
+++ src/A_ClassAndData/ClassStart01.java
@@ -0,0 +1,18 @@
+package A_ClassAndData;
+
+public class ClassStart01 {
+
+    public static void main(String[] args) {
+
+        String studentName1 = "학생1";
+        int age1 = 15;
+        int grade1 = 90;
+
+        String studentName2 = "학생2";
+        int age2 = 16;
+        int grade2 = 80;
+
+        System.out.println("이름 : " + studentName1 + " 나이 : " + age1 + " 성적 : " + grade1);
+        System.out.println("이름 : " + studentName2 + " 나이 : " + age2 + " 성적 : " + grade2);
+    }
+}
