daef4ce0af775c2463f475ff98222c292f66367a
OurOh <ahn980321@gmail.com>
2024-09-15 21:02:47 +0900

    0915 commit

---
+++ src/I_extend/access/ExtendsAccessMain.java
@@ -0,0 +1,12 @@
+package I_extend.access;
+
+import I_extend.access.child.Child;
+
+public class ExtendsAccessMain {
+
+    public static void main(String[] args) {
+
+        Child child = new Child();
+        child.call();
+    }
+}
--- src/I_extend/access/child/Child.java
+++ src/I_extend/access/child/Child.java
@@ -3,4 +3,18 @@ package I_extend.access.child;
 import I_extend.access.parent.Parent;
 
 public class Child extends Parent {
+
+    public void call() {
+        publicValue = 1;
+        protectedValue = 1;
+        //defaultValue = 1;
+        //privateValue = 1;
+
+        publicMethod();
+        protectedMethod();
+        //defaultMethod();
+        //privateMethod();
+
+        printParent();
+    }
 }
